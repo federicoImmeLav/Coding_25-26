c84b246af70fc658438e4c864d2fc8ea218098de
federicoImmeLav <117451142+federicoImmeLav@users.noreply.github.com>
2025-09-30 09:26:05 +0200

    prova javascript

---
+++ 3D/02-javascript/script.js
@@ -0,0 +1,19 @@
+console.log("hello");
+
+function  nero() {
+document.getElementById("quadrato").style.backgroundColor = "black";
+
+
+
+}
+function  blue() {
+document.getElementById("quadrato").style.backgroundColor = "blue";
+}
+
+function  yellow() {
+document.getElementById("quadrato").style.backgroundColor = "yellow";
+}
+
+function  pink() {
+document.getElementById("quadrato").style.backgroundColor = "pink";
+}
