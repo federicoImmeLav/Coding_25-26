ca7b64f6f60987b7050ae26563221e941277b535
federicoImmeLav <117451142+federicoImmeLav@users.noreply.github.com>
2025-10-17 11:17:08 +0200

    lezioni python

---
+++ Python/2D/01-intro.py
@@ -0,0 +1,17 @@
+# nome = "Federico" #le scritte si chiamano stringhe
+# anno = 1995 #numero intero (integer)
+# gradi = 21.4 #numero decimale (float)
+# booleana = True #booleane True/False
+
+# cognome = "Pozzi"
+
+# # print("Mi chiamo " + nome + " " + cognome)
+# # #modo per unire più smart
+# print(f"Mi chiamo {nome} {cognome}")
+
+nome = input("Come ti chiami? ")
+#se chiedo un numero metto int davanti a input
+anno = int(input("In che anno sei nato? "))
+print(f"Ti chiami {nome} e sei nato nel {anno}")
+
+
---
+++ Python/3D/03-comandiBase.py
@@ -0,0 +1,28 @@
+# # nome = input("come ti chiami? ")
+# # print(nome)
+# # eta =int(input("quanti anni hai? "))
+# # print(f"mi chiamo {nome} e ho {eta} anni ")
+
+# # # cognome = input( "qual è il tuo cognome? ")
+# # # print(cognome)
+
+# # anno = 2025 - eta
+# # print(f"{nome} è nato nel {anno} ")
+
+
+
+
+# altezza = float(input("quanto sei alto? "))
+# print(altezza)
+# if altezza >= 1.50:
+#     print("sei più alto di 1.50")
+# elif altezza < 1.50:
+#     print("sei più basso di 1.50 ")
+
+# voto = int(input("Qual è l'ultimo voto che hai preso? "))
+# if voto >= 80:
+#     print("sei bravissimo! ")
+# elif voto >= 60:
+#     print("Sei bravo!")
+# else:
+#     print("Meh!! ")
